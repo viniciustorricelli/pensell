3747989d95872ffc6504823bf808e8f3eaa1826b
Vinicius Torricelli <vinicius.ts16@gmail.com>
2026-01-09 21:18:33 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -310,8 +310,8 @@ export default function Layout({ children, currentPageName }) {
               </DropdownMenu>
             }
             
-            <Link to={createPageUrl('Home')} className="flex items-center gap-2">
-              <img src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695b603a6d9b50e34c2be229/b9a529274_1-removebg-preview.png" alt="PenSell" className="h-6" />
+            <Link to={createPageUrl('Home')} className="flex flex-1 items-center justify-center">
+              <img src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695b603a6d9b50e34c2be229/b9a529274_1-removebg-preview.png" alt="PenSell" className="h-10" />
             </Link>
           </div>
 
