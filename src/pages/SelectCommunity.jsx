19d901650efaafc85e275d95657f2db7ec1c9bb0
Vinicius Torricelli <vinicius.ts16@gmail.com>
2026-01-09 20:40:14 +0000

    File changes

--- src/pages/SelectCommunity.jsx
+++ src/pages/SelectCommunity.jsx
@@ -95,7 +95,7 @@ export default function SelectCommunity() {
               </h1>
 
           {/* Search */}
-          <div className="relative mb-4">
+          <div className="relative mb-3">
             <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
             <Input
               placeholder="Pesquisar instituição"
